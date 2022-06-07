aac8aca7706221f8d3d6cc9b9a914a9e6a60b3c9
user1 <user1@DESKTOP-LE5AAG8>
2022-06-07 14:37:57 +0900

    7

--- JavaBasic/src/java04_control/condition/IfElse_03.java
+++ JavaBasic/src/java04_control/condition/IfElse_03.java
@@ -1,6 +1,6 @@
 package java04_control.condition;
 
-// 들여쓰기 정렬 단축키 : ctrl +i
+// 들여쓰기 정렬 단축키 : ctrl +i 전체 선택 한 후에
 public class IfElse_03 {
 	public static void main(String[] args) {
 
@@ -21,6 +21,14 @@ public class IfElse_03 {
 
 			}
 			System.out.println("0보다 크다");
+		} else {
+			System.out.println("0보다 크지 않다");
+
+			if ( num == 0) {
+				System.out.println("0과 같다");
+			} else {
+				System.out.println("0보다 작다");
+			}
 		}
 	}
 
